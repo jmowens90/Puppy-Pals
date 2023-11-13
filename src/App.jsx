f1d76b4550f87cd4d063dff96a74fbb42b376a95
Jaylen <jmowens90@gmail.com>
2023-11-13 15:52:57 -0500

    added the imports from react and mapped through the puppyList to show a list of puppy names

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import './App.css'
+import { useState } from 'react';
+import { puppyList } from './data';
+import './App.css';
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList);
+console.log(puppies);
   return (
-    <>
-      <div></div>
-    </>
+      <div>
+        { puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })}
+      </div>
   )
 }
 
