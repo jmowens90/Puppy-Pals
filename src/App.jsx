4cc5f1bff7df1617730e7fbdad6a976a4a89f430
Jaylen <jmowens90@gmail.com>
2023-11-13 16:51:39 -0500

    final commit to make sure all changes were caught

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from 'react';
 import { puppyList } from './data';
+import './index.css';
 import './App.css';
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-console.log(puppies);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
   return (
       <div>
         { puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>;
         })}
+        <p> {featPupId && (
+              <div>
+                <h2>{featuredPup.name}</h2>
+                <ul>
+                  <li>Age: {featuredPup.age}</li>
+                  <li>Email: {featuredPup.email}</li>
+                </ul>
+              </div>)}
+        </p>
       </div>
   )
 }
